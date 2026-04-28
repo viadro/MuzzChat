23a4f3931af82a7318c5a836a1987559fddb8b9d
piotr <piotr.seweryn.4@gmail.com>
2026-04-28 21:55:11 +0200

    Removed unused imports

--- feature-chat/src/main/java/com/seweryn/chat/presentation/ui/ChatScreen.kt
+++ feature-chat/src/main/java/com/seweryn/chat/presentation/ui/ChatScreen.kt
@@ -1,10 +1,8 @@
 package com.seweryn.chat.presentation.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
