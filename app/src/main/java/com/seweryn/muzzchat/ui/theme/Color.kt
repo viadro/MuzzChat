234d1435d58552b903257feb91d76f260d50d738
piotr <piotr.seweryn.4@gmail.com>
2026-04-29 20:45:53 +0200

    Colors

--- app/src/main/java/com/seweryn/muzzchat/ui/theme/Color.kt
+++ app/src/main/java/com/seweryn/muzzchat/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.seweryn.muzzchat.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPink = Color(0xFFFF4D9D)
+val DeepBlue = Color(0xFF1E2630)
+val OffWhite = Color(0xFFB8C2CF)
+val DarkNight = Color(0xFF121212)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BrightPink = Color(0xFFFF1479)
+val LightGrey = Color(0xFFF2F5FB)
+val DarkGrey = Color(0xFF3E4D5D)
