0193eb8a0f1215a89cc31b75ee72dcd96094c4b7
piotr <piotr.seweryn.4@gmail.com>
2026-04-29 19:37:42 +0200

    Scrolling to the bottom of the chat on new message

--- feature-chat/src/main/java/com/seweryn/chat/presentation/ui/ChatScreen.kt
+++ feature-chat/src/main/java/com/seweryn/chat/presentation/ui/ChatScreen.kt
@@ -15,12 +15,14 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -115,12 +117,19 @@ private fun ChatMessagesContent(
     modifier: Modifier,
     items: List<ChatItem>
 ) {
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(items.size) {
+        listState.animateScrollToItem(0)
+    }
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .then(modifier),
         contentPadding = PaddingValues(8.dp),
         reverseLayout = true,
+        state = listState,
     ) {
         items(items, key = { it.key }) { item ->
             when (item) {
